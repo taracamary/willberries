850d5530ed870cc64f27a8362c0d5d3f47217fec
Maria Tarasenko <taracamary@gmail.com>
2023-03-06 17:09:07 +0300

    add swiper slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    loop: true, // бесконечный слайдер
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+});
